cbbc12825a01f00c5eed6f1761c473d56d5f8aa3
Scott Healy <scott.mik.healy@gmail.com>
2016-03-18 23:32:41 -0400

    finished #4

--- rankall.R
+++ rankall.R
@@ -17,68 +17,62 @@ rankall <- function(outcome, num = "best") {
   if(!file.exists(dataFile)) stop("no file found")
   dtafrm <- read.csv(dataFile)
   
-  message(head(dtafrm, 2))
   ## connect outcome with dot
   outcome <- gsub(" ", ".", outcome)
-  ## Check that state is valid
   ## Check that state and outcome are valid
   outcomeColName <-  grep(paste("^", "Hospital.30.Day.Death.", ".*", outcome, "$", sep=""),  names(dtafrm), value = TRUE, ignore.case = TRUE)
   if(!length(outcomeColName)) stop("invalid outcome")
+
+  ## cast as numeric
+  dtafrm[,outcomeColName] <- suppressWarnings(as.numeric(as.character(dtafrm[,outcomeColName])))
   
+  ## order the frame
+  dtafrm <- with(dtafrm, dtafrm[order(dtafrm[,outcomeColName], dtafrm$Hospital.Name, decreasing = FALSE),])
   
-  ##subset with cols
-  ##dtafrm[,c("Hospital.Name", "State", outcomeColName)]
-  ##cast as numeric
-  ##dtafrm[,outcomeColName] <- suppressWarnings(as.numeric(as.character(dtafrm[,outcomeColName])))
-  ##remove na cases
-  ##dtafrm <- dtafrm[complete.cases(dtafrm[, c("Hospital.Name", "State", outcomeColName)]),];
+  ## subset to the name and state
+  dtafrm2 <- dtafrm[, c("Hospital.Name", "State")]
   
-  dtafrm
-  
-  # isWorst = FALSE
-  # ## check for string "best" || "worst"
-  # if(identical(num , "best")){
-  #   num = 1
-  # }else if(identical(num , "worst")){
-  #   isWorst = TRUE
-  # }else{
-  #   ## cast as int or na
-  #   num = as.integer(num)
-  #   if(is.na(num)) stop("invalid num")
-  # }
-  # 
-  # ## For each state, find the hospital of the given rank
-  # states <- split(dtafrm, dtafrm$State)
-  # 
-  # head(states)
-  # 
+  isWorst = FALSE
+  ## check for string "best" || "worst"
+  if(identical(num , "best")){
+    num = 1
+  }else if(identical(num , "worst")){
+    isWorst = TRUE
+  }else{
+    ## cast as int or na
+    num = as.integer(num)
+    if(is.na(num)) stop("invalid num")
+  }
+
+  ## For each state, find the hospital of the given rank
+  states <- split(dtafrm2, dtafrm$State)
   
   ##iterate and create df of states
-  # returnFrame <- NULL;
-  # for(state in states) {
-  #   
-  #   if(!isTRUE(which(state[1,"State"]%in%state.abb) > 0)){
-  #     next()
-  #   }
-  #   
-  #   ## message(state[num,"State"])
-  #   ## message(state[num,outcomeColName])
-  #   ## if it is worst we find the last row of the state
-  #   if(isWorst) {
-  #     row <- state[nrow(state),]
-  #   }else{
-  #     row <- state[num,]
-  #   }
-  #   rbind(returnFrame,row)->returnFrame
-  #   
-  # }
-  # 
-  # str(returnFrame)
+  returnFrame <- data.frame("hospital" = character(), "state" = character(), stringsAsFactors = FALSE);
   
+  ##iterate the split DF
+  for(state in states) {
 
-  
-  
+    ## if it is worst we find the last row of the state
+    if(isWorst) {
+      num = nrow(state)
+    }
+    
+    ## check that the state has this many hospitals to rank
+    if(nrow(state) < num){
+      row <- data.frame( "hospital"=NA, "state"=state[1, 'State'], row.names = state[1, 'State'] )
+    } else {
+      row <- data.frame(  "hospital"=state[num,"Hospital.Name"],  "state"=state[num,"State"], row.names = state[1, 'State']);
+    }
+    
+    ## add back to the data frame
+    returnFrame <- rbind(returnFrame,row)
+
+  }
+
   ## Return a data frame with the hospital names and the
-  
   ## (abbreviated) state name
+  
+  returnFrame
+
 }
